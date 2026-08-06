1a0ef930c803fa7bb59c76a171b1ed8834857b58
hanjianfei <627772669@qq.com>
2026-08-07 07:51:49 +0800

    refactor(app): 重写 app.dart 为 MaterialApp.router,删除旧单体 AppShell

--- apps/desktop/lib/main.dart
+++ apps/desktop/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:media_kit/media_kit.dart';
 
 import 'app.dart';
 
 void main() {
   MediaKit.ensureInitialized();
-  runApp(const BasketballHighlightApp());
+  runApp(const ProviderScope(child: CourtsideApp()));
 }
